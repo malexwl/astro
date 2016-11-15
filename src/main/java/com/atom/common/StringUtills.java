937b5bdbfae8048718679c1b0fb638ddee859fd1
malexwl <malexwl@yandex.ru>
2016-11-15 22:59:44 +0300

    added util methods

--- src/main/java/com/atom/common/StringUtills.java
+++ src/main/java/com/atom/common/StringUtills.java
@@ -5,4 +5,15 @@ package com.atom.common;
  */
 public class StringUtills {
     public static final String EMPTY = "";
+
+    private StringUtills() {
+    }
+
+    public static boolean isEmpty(String s) {
+        return s == null || s.length() == 0;
+    }
+
+    public static boolean isNotEmpty(String s) {
+        return !isEmpty(s);
+    }
 }
