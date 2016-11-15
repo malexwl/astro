53622bce29b926aabd4c6b6416196c4e338a81a8
malexwl <malexwl@yandex.ru>
2016-11-15 23:02:44 +0300

    added login scenario

--- src/main/java/com/atom/scenario/AbstractScenario.java
+++ src/main/java/com/atom/scenario/AbstractScenario.java
@@ -1,24 +1,42 @@
 package com.atom.scenario;
 
 import com.atom.ApplicationContext;
+import com.atom.bean.CriticalError;
+import com.atom.bean.json.login.LoginResponse;
 import com.atom.builders.RequestBuilder;
 import com.atom.common.RequestType;
+import com.atom.common.StringUtills;
 import com.atom.interfaces.Configuration;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public abstract class AbstractScenario {
 
     protected Configuration configuration;
     protected RequestBuilder requestBuilder;
+    protected Gson gson;
 
     public AbstractScenario() {
         configuration = ApplicationContext.getInstance().getConfiguration();
         requestBuilder = ApplicationContext.getInstance().getRequestBuilder();
+        gson = new GsonBuilder().create();
     }
 
     public abstract void doScenario();
 
     public void doLogin() {
         String responseStr = requestBuilder.buildRequest(RequestType.LOGIN);
-
+        if (StringUtills.isNotEmpty(responseStr)) {
+            LoginResponse loginResponse = gson.fromJson(responseStr, LoginResponse.class);
+            try {
+                String key = loginResponse.getData().getKey();
+                if (StringUtills.isEmpty(key)) {
+                    throw new CriticalError("Cannot read \"key\" param at login");
+                }
+                configuration.getLoginData().setKey(key);
+            } catch (NullPointerException exc) {
+                throw new CriticalError("Cannot process login response");
+            }
+        }
     }
 }
